7eb14458fcddc494876508d58bb85a2555409c27
new12122212 <new12122212@gmail.com>
2025-08-26 14:15:02 -0400

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -85,4 +86,20 @@ def home():
     """
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+          }
+        </style>
+      </head>
+      <body>
+        <div class="heart">
+          <div class="name">Gissel</div>
+        </div>
+      </body>
+    </html>
+    """
+
+if __name__ == "__main__":
     app.run(debug=True)
+
