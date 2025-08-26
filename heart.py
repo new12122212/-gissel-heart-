7907eda5801323b31ea64f77b0a2e2a6777174a0
new12122212 <new12122212@gmail.com>
2025-08-26 14:00:03 -0400

    Add files via upload

--- heart.py
+++ heart.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)  # <-- make sure it has the underscores!
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -17,40 +17,62 @@ def home():
             background-color: pink;
             margin: 0;
           }
+
           .heart {
             position: relative;
-            width: 100px;
-            height: 90px;
+            width: 150px;
+            height: 135px;
             background: red;
             transform: rotate(-45deg);
             margin: 0 auto;
+            animation: beat 1s infinite;
           }
+
           .heart::before,
           .heart::after {
             content: "";
             position: absolute;
-            width: 100px;
-            height: 100px;
+            width: 150px;
+            height: 150px;
             background: red;
             border-radius: 50%;
           }
+
           .heart::before {
-            top: -50px;
+            top: -75px;
             left: 0;
           }
+
           .heart::after {
-            left: 50px;
+            left: 75px;
             top: 0;
           }
+
           .name {
             position: absolute;
-            top: 35%;
+            top: 50%;
             left: 50%;
-            transform: translate(-50%, -50%) rotate(45deg);
+            transform: translate(-50%, -50%) rotate(45deg); /* undo heart rotation */
             color: white;
             font-family: Arial, sans-serif;
             font-weight: bold;
-            font-size: 20px;
+            font-size: 24px;
+            white-space: nowrap;
+          }
+
+          @keyframes beat {
+            0%, 100% {
+              transform: rotate(-45deg) scale(1);
+            }
+            25% {
+              transform: rotate(-45deg) scale(1.1);
+            }
+            50% {
+              transform: rotate(-45deg) scale(1.2);
+            }
+            75% {
+              transform: rotate(-45deg) scale(1.1);
+            }
           }
         </style>
       </head>
